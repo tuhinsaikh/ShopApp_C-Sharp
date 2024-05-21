4de711d6cf2c2374a7a6d827b6b0ed93bd2d896f
tsaikh <tsaikh@entiovi.com>
2024-05-21 11:06:46 +0530

    arch added

---
+++ ShopWeb.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopWeb.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> predicate);
+        void Remove(T entity);
+        void Add(T entity);
+        void RemoveRenge(IEnumerable<T> entities);
+    }
+}
